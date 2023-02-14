e88b821686553ca69892def3b2a2a01731dbd3f6
ali salehi <sali.up44@gmail.com>
2023-02-15 00:42:48 +0330

    new commit

--- src/myprojects.js
+++ src/myprojects.js
@@ -1,41 +1,41 @@
 import tenzies from "./img/Tenzies.png"
 import weatherphoto from "./img/Weather.png"
-import memegenerator from"./img/MemeGenerator.png"
+// import memegenerator from"./img/MemeGenerator.png"
 import shop from "./img/shop.png"
 import rosso from "./img/rosso.png"
+import thirdteacher from "./img/thirdteacher.png"
 
 let projects = [
-{
-    img:weatherphoto,
-    name:"simple weather website",
-    information:"Java script/Html/Css/Api",
-    link:"https://github.com/sali2313/Weather"
-},
-,
-{
-    img:tenzies,
-    name:"Tenzies",
-    information:"React, css",
-    link:"https://github.com/sali2313/Tenzies"
-},
-{
-    img:memegenerator,
-    name:"Meme generator",
-    information:"My first REACT webiste.",
-    link:"https://github.com/sali2313/MemeGenerator"
-},
-{
+    {
+        img:rosso,
+        name:"Rossoarch",
+        information:"fixing bugs",
+        link:"https://rosso-2.vercel.app/"
+    },
+    {
     img:shop,
     name:"Clothes Shop",
     information:"React-Context Api-React icons-Api",
     link:"https://online-shop-46162.web.app"
-},
-{
-    img:rosso,
-    name:"Rossoarch",
-    information:"React - Next",
-    link:"https://rosso-2.vercel.app/"
-}
+    },
+    {
+    img: thirdteacher,
+    name:"Third Teacher",
+    information:"React",
+    link:"http://demo.thethirdteacher.ir/home/schools"
+    },
+    {
+    img:weatherphoto,
+    name:"simple weather website",
+    information:"Java script/Html/Css/Api",
+    link:"https://github.com/sali2313/Weather"
+    },
+    {
+    img:tenzies,
+    name:"Tenzies",
+    information:"React, css",
+    link:"https://github.com/sali2313/Tenzies"
+    }
 ]
 
 export default projects
